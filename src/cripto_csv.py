405f254de1ab5ee60f4ad2fdfaf633fc16f78851
Renan Domingos <rna.domingos@hotmail.com>
2025-05-01 14:12:25 -0300

    feat: add encryption csv

---
+++ src/cripto_csv.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from hashlib import sha256
+
+file_csv = './exemplo.csv'
+
+df = pd.read_csv(file_csv)
+
+exclude_columns = []
+# Iterate through each row and column
+for index, row in df.iterrows():
+    for column in df.columns:
+        # Check if the column is in the exclusion list
+        if column not in exclude_columns:
+            # Get the value from the current cell
+            value = str(row[column])
+
+        # Hash the value using SHA-256
+        hashed_value = sha256(value.encode()).hexdigest()
+
+        # Update the DataFrame with the hashed value
+        df[column] = df[column].astype(str) 
+        df.at[index, column] = hashed_value
+# Path to save the hashed CSV file
+hashed_file_path = "hashed_data.csv"
+
+# Save the modified DataFrame to a new CSV file
+df.to_csv(hashed_file_path, index=False)
